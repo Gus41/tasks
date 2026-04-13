6bc9093fbfd2bf41d4d459fc7f867dbe5aaddb37
Gustavo <123191793+Gus41@users.noreply.github.com>
2026-04-13 18:56:00 +0000

    fix: final adjustments

--- backend/TodoApi/Program.cs
+++ backend/TodoApi/Program.cs
@@ -18,11 +18,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+ 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
+app.UseCors("AllowAll");
 app.MapControllers();
 
 app.Run();
